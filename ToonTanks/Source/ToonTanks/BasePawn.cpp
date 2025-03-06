a910d84217ec1aa811b3cc5c297832c69d2ef077
yeomin <jun1778@naver.com>
2025-03-06 11:46:25 +0900

    116: Constructing The Capsule
    - CreateDefaultSubobject : 생성자에서 오브젝트를 만들 때 사용
    - 예시 : CapsuleComp = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule Collider"));
    - 만든 것을 루트 컴포넌트로 지정했다
    - RootComponent = CapsuleComp;

--- ToonTanks/Source/ToonTanks/BasePawn.cpp
+++ ToonTanks/Source/ToonTanks/BasePawn.cpp
@@ -2,12 +2,17 @@
 
 
 #include "BasePawn.h"
+#include "Components/CapsuleComponent.h"
 
 // Sets default values
 ABasePawn::ABasePawn()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	// 생성자에서 오브젝트 생성 
+	CapsuleComp = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule Collider"));
+	RootComponent = CapsuleComp;	// 루트 컴포넌트로 지정 
 
 }
 
