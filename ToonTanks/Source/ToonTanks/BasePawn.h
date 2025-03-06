96573eed558332814ae41d15468498bb8b629c63
yeomin <jun1778@naver.com>
2025-03-06 11:38:57 +0900

    115: Forward Declaration
    - 헤더 파일을 포함시키는 행위가 컴파일 시간을 증가시키기 때문에 이를 막기 위해 포인터 객체를 선언할 때는 클래스 선언 전에 필요한 클래스를 명시하여 헤더파일의 중복을 막는다
    - 포인터는 다른 포인터와 똑같이 `주소` 를 가져오기 때문에 크기가 작다

--- ToonTanks/Source/ToonTanks/BasePawn.h
+++ ToonTanks/Source/ToonTanks/BasePawn.h
@@ -19,6 +19,11 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+private:	// 이 클래스에서만 접근 가능
+	UPROPERTY()
+	class UCapsuleComponent* CapsuleComp;
+	// forward declaration (전방선언) : 헤더 파일을 포함시키는 행위가 컴파일 시간을 증가시키기 때문에 이를 막기 위해 포인터 객체를 선언할 때는 클래스 선언 전에 필요한 클래스를 명시하여 헤더파일의 중복을 막는다
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
