988cb279a93c9df901050661e2a4655b9d464ed0
yeomin <jun1778@naver.com>
2025-03-07 12:10:56 +0900

    138: Health HealthComponent
    - UActorComponent vs USceneComponent
    - UActorComponent는 USceneComponent와 달리 Transform과 attachment가 없다. 그러므로 Health Component로 적절하다.
    
    - HealthComponent 클래스를 만들고 	void DamageTaken(AActor* DamagedActor, float Damage, const UDamageType* DamageType, class AController* Instigator, AActor* DamageCauser);
    콜백 함수를 만들었다.
    - GetOwner()->OnTakeAnyDamage.AddDynamic 델리게이트 등록까지 마쳤다.

---
+++ ToonTanks/Source/ToonTanks/HealthComponent.cpp
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HealthComponent.h"
+
+// Sets default values for this component's properties
+UHealthComponent::UHealthComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UHealthComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// 초기 생명력 최대로 초기화 
+	Health = MaxHealth;
+
+	// Damage 감지 델리게이트 등록 
+	GetOwner()->OnTakeAnyDamage.AddDynamic(this, &UHealthComponent::DamageTaken);
+}
+
+
+// Called every frame
+void UHealthComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
+void UHealthComponent::DamageTaken(AActor* DamagedActor, float Damage, const UDamageType* DamageType, class AController* Instigator, AActor* DamageCauser)
+{
+	
+}
---
+++ ToonTanks/Source/ToonTanks/HealthComponent.h
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "HealthComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class TOONTANKS_API UHealthComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UHealthComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(EditAnywhere)
+	float MaxHealth = 100.f;
+	float Health = 0.f;
+
+	// DamagedActor : 데미지 입은 액터
+	// UDamageType : 언리얼에서 제공하는 타입 (독, 폭발 등... 커스텀도 가능)
+	// InstigatedActor : 특정 액션이나 이벤트를 일으킨 주체
+	// DamageCauser : Projectile
+	UFUNCTION()
+	void DamageTaken(AActor* DamagedActor, float Damage, const UDamageType* DamageType, class AController* Instigator, AActor* DamageCauser);
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+		
+};
