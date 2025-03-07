90ce5b277798dbb2820d211196dcaec6d5229d0c
yeomin <jun1778@naver.com>
2025-03-07 11:07:13 +0900

    136: Projectile Movement Component
    - Projectile이 발사되어 날아가도록 움직임 추가
    - Tower에서는 2초마다 사정거리에 Tank가 있으면 총알이 발사되어 날아가도록, Tank는 마우스가 클릭한 방향으로 총알이 날아가도록 움직임 추가.
    - 문제점 : 총알이 계속 남아있어 기존의 움직임에 방해가 됨. Collision 설정을 변경하든, 맞추지 못했을 시 사라지게 하든 수정이 필요할 듯.

--- ToonTanks/Source/ToonTanks/Projectile.cpp
+++ ToonTanks/Source/ToonTanks/Projectile.cpp
@@ -3,16 +3,21 @@
 
 #include "Projectile.h"
 #include "Components/StaticMeshComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
 
 // Sets default values
 AProjectile::AProjectile()
 {
 	PrimaryActorTick.bCanEverTick = false;	// Projectile은 굳이 매 초마다 부를 필요가 없으니 Tick은 false로
-
-
+	
 	// Mesh 설정
 	ProjectileMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Projectile Mesh"));
 	RootComponent = ProjectileMesh;
+
+	// 발사체 움직임 설정. 초기 속도 = 최대 속도 
+	ProjectileMovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("Projectile Movement Component"));
+	ProjectileMovementComponent->MaxSpeed = 1300.f;
+	ProjectileMovementComponent->InitialSpeed = 1300.f;
 }
 
 // Called when the game starts or when spawned
